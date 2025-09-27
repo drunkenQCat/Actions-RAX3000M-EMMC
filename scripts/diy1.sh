d1143a4fc02ecfe03d7994c614e7bc46517ea05c
drunkenQCat <39608175+drunkenQCat@users.noreply.github.com>
2025-09-28 06:25:26 +0800

    使用OpenClash和OpenAppFilter

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -3,5 +3,9 @@
 # Add a feed source
 
 cd package
-git clone https://github.com/xiaorouji/openwrt-passwall
+git clone https://github.com/vernesong/OpenClash.git
+pushd OpenClash/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+git clone https://github.com/destan19/OpenAppFilter.git
 git clone https://github.com/kenzok78/luci-app-adguardhome
